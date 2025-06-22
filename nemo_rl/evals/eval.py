469ad59c1db70f45ad8a5f0a7ca8a0697c27061a
Dheeraj Peri <peri.dheeraj@gmail.com>
2025-06-22 21:48:25 +0000

    feat: Implement pass@k metric
    Signed-off-by: Dheeraj Peri <peri.dheeraj@gmail.com>

--- nemo_rl/evals/eval.py
+++ nemo_rl/evals/eval.py
@@ -16,6 +16,7 @@ import os
 from typing import TypedDict
 
 import ray
+import torch
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
 
@@ -26,8 +27,7 @@ from nemo_rl.data.llm_message_utils import get_keys_from_message_log
 from nemo_rl.distributed.batched_data_dict import BatchedDataDict
 from nemo_rl.distributed.virtual_cluster import ClusterConfig, RayVirtualCluster
 from nemo_rl.environments.math_environment import MathEnvConfig
-from nemo_rl.models.generation.interfaces import GenerationConfig
-from nemo_rl.models.generation.vllm import VllmGeneration
+from nemo_rl.models.generation.vllm import VllmConfig, VllmGeneration
 
 # ===============================================================================
 # Configuration
@@ -38,11 +38,18 @@ class EvalConfig(TypedDict):
     metric: str
     num_tests_per_prompt: int
     seed: int
+    pass_k_value: int
+
+
+class EvalGenerationConfig(VllmConfig):
+    """Extended VllmConfig for evaluation with additional fields."""
+
+    num_prompts_per_step: int
 
 
 class MasterConfig(TypedDict):
     eval: EvalConfig
-    generate: GenerationConfig
+    generation: EvalGenerationConfig
     data: MathDataConfig
     env: MathEnvConfig
     cluster: ClusterConfig
@@ -83,15 +90,25 @@ def setup(
 
     # Check settings
     metric = eval_config["metric"]
+    pass_k_value = eval_config["pass_k_value"]
     num_tests_per_prompt = eval_config["num_tests_per_prompt"]
     temperature = generation_config["temperature"]
     top_k = generation_config["top_k"]
-    # TODO @yukih: support pass@k and cons@k
-    assert metric in ["pass@1"], f"Invalid metric: {metric}"
+    # TODO @yukih: support cons@k
+    assert metric in ["pass@1", "pass@k"], f"Invalid metric: {metric}"
     if num_tests_per_prompt > 1:
         assert temperature > 0 and top_k != 1, (
             "temperature > 0 and top_k != 1 are required for multiple samples"
         )
+
+    # Validate metrics
+    if metric == "pass@k":
+        assert pass_k_value > 1, "pass_k_value must be greater than 1 for pass@k metric"
+        assert num_tests_per_prompt >= pass_k_value, (
+            "num_tests_per_prompt must be greater than or equal to pass_k_value for pass@k metric"
+        )
+    elif metric == "pass@1":
+        assert pass_k_value == 1, "pass_k_value must be 1 for pass@1 metric"
 
     # ==========================
     #           Data
@@ -99,7 +116,7 @@ def setup(
     if generation_config["num_prompts_per_step"] == -1:
         generation_config["num_prompts_per_step"] = len(dataset)
     dataloader = DataLoader(
-        dataset,
+        dataset,  # type: ignore
         batch_size=generation_config["num_prompts_per_step"],
         shuffle=False,
         collate_fn=eval_collate_fn,
@@ -150,6 +167,40 @@ def setup(
 # ===============================================================================
 
 
+def eval_pass_k(rewards: torch.Tensor, num_tests_per_prompt: int, k: int) -> float:
+    """Evaluate pass@k score using an unbiased estimator.
+
+    Reference: https://github.com/huggingface/evaluate/blob/32546aafec25cdc2a5d7dd9f941fc5be56ba122f/metrics/code_eval/code_eval.py#L198-L213
+
+    Args:
+        rewards: Tensor of shape (batch_size, num_tests_per_prompt)
+        k: int (pass@k value)
+
+    Returns:
+        pass_k_score: float
+    """
+    # rewards is a 1d tensor of size (batch_size * num_tests_per_prompt)
+    chunks = rewards.split(num_tests_per_prompt)
+    pass_k_score = 0.0
+    for chunk in chunks:
+        num_correct = chunk.sum().item()
+        if num_tests_per_prompt - num_correct < k:
+            pass_k_score += 1.0
+        else:
+            pass_k_score += float(
+                1.0
+                - torch.prod(
+                    1.0
+                    - k
+                    / torch.arange(
+                        num_tests_per_prompt - num_correct + 1, num_tests_per_prompt + 1
+                    )
+                ).item()
+            )
+
+    return pass_k_score
+
+
 def run_env_eval(vllm_generation, dataloader, env, master_config):
     """Main entry point for running evaluation using environment.
 
@@ -166,12 +217,18 @@ def run_env_eval(vllm_generation, dataloader, env, master_config):
     eval_config = master_config["eval"]
     metric = eval_config["metric"]
     num_tests_per_prompt = eval_config["num_tests_per_prompt"]
+    pass_k_value = eval_config["pass_k_value"]
 
     # Run evaluation loop
     score, count = 0.0, 0
     for batch in dataloader:
         # update stats
-        count += batch.size * num_tests_per_prompt
+        if metric == "pass@1":
+            count += batch.size * num_tests_per_prompt
+        elif metric == "pass@k":
+            count += batch.size
+        else:
+            raise ValueError(f"Invalid metric: {metric}")
 
         # measure multiple samples
         if num_tests_per_prompt > 1:
@@ -203,10 +260,12 @@ def run_env_eval(vllm_generation, dataloader, env, master_config):
             for i in range(len(batch["message_log"]))
         ]
         env_return = ray.get(env.step.remote(to_env, batch["extra_env_info"]))
-
+        rewards = env_return.rewards
         # update stats
         if metric == "pass@1":
-            score += env_return.rewards.sum().item()
+            score += rewards.sum().item()
+        elif metric == "pass@k":
+            score += eval_pass_k(rewards, num_tests_per_prompt, pass_k_value)
         else:
             raise ValueError(f"Invalid metric: {metric}")
 
