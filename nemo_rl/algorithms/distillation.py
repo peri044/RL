1008fda95c0b33e3e778f63ba900d37d668fc851
ashors1 <ashors@nvidia.com>
2025-10-09 21:13:28 -0700

    default to None
    Signed-off-by: ashors1 <ashors@nvidia.com>

--- nemo_rl/algorithms/distillation.py
+++ nemo_rl/algorithms/distillation.py
@@ -413,7 +413,7 @@ def setup(
         generation_config = cast(VllmConfig, generation_config)
         ## make vllm hf overrides match the training policy
         generation_config["vllm_cfg"]["hf_overrides"] = policy_config.get(
-            "hf_config_overrides", {}
+            "hf_config_overrides"
         )
         student_generation = VllmGeneration(
             cluster=inference_cluster, config=generation_config
